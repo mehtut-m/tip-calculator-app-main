a2881a4b5e8f9fe95ab1859df4719918cb3b247d
M. M <mehtut.m@gmail.com>
2021-11-09 12:53:08 +0700

    version 1.0
    update the calculation function.

--- index.js
+++ index.js
@@ -1,39 +1,113 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Application Started")
+    //
+    const tipRateButton = document.querySelectorAll('.tip-rate').forEach(element => element.addEventListener('click', activeTipRate))
+    //Initiate Application on pressing key
+    document.querySelectorAll('input[type=number]').forEach(element => element.addEventListener('keyup', initiateApp));
     
-    const tipRateButton = document.querySelectorAll('button').forEach(element => element.addEventListener('click', getTipRate))
-
-    let test = document.querySelectorAll('input[type=number]').forEach(element => element.addEventListener('keyup', calculateTip));
-
+    //add reset functopn to reset button
     document.querySelector('.reset-button').addEventListener('click', resetAmount);
-    function getTipRate() {
+    
+    resetDisable();
+
+    //Main Calculator Control 
+    function initiateApp () {
+        console.log('Calculating')
+        resetEnable();
+        let billAmount = getPrice();    
+        let peopleCount = getPeople();
+        let tipRate = getTipRate();
+        
+        // console.log(billAmount)
+        // console.log(peopleCount)
+        // console.log(tipRate)
+
+        if(inputVerify(billAmount, peopleCount, tipRate)) {
+            //start calculation
+            let calcAmount = calculateTip(billAmount, peopleCount, tipRate.value);
+            //update ui
+            updateAmount(calcAmount.grandTotal, calcAmount.amountPerPerson)
+        }
+        return ;
+    }
+
+
+    function activeTipRate() {
         clearActiveRate();
         this.classList.toggle('active-rate');
-        return this.value;
+        initiateApp();
     }
+    function getPrice() {
+        return document.querySelector('.bill-amount').value;
+    }
+    function getPeople() {
+        return document.querySelector('.people-count').value;
+    }
+    function getTipRate() {
+        return document.querySelector('.active-rate');
+    }
+
+
+    
+    function inputVerify (billAmount, peopleCount, tipRate) {
+        if (billAmount == null || billAmount == undefined || billAmount == "") {
+            return false
+        }
+        else if (peopleCount == null || peopleCount == undefined || peopleCount == "" ) {
+            
+            return false
+        }
+        else if (tipRate == undefined || tipRate == null) {
+            return false
+        }
+        else if (tipRate.value == "") {
+            return false
+
+        }
+        return true;
+    }
+    function resetEnable () {
+        document.querySelector('.reset-button').disabled = false;
+    }
+    function resetDisable () {
+        document.querySelector('.reset-button').disabled = true;
+    }
+
+    function calculateTip(billAmount, peopleCount, tipRate) {
+        billAmount = parseInt(billAmount);    
+        peopleCount = parseInt(peopleCount);
+        tipRate = parseInt(tipRate);
+        
+        let grandTotal = billAmount * (1 + tipRate/100);
+        let amountPerPerson = grandTotal/peopleCount
+        
+        return {
+            grandTotal: grandTotal,
+            amountPerPerson: amountPerPerson
+        }
+    }
+
+    //Update the User Interface
     function clearActiveRate() {
         let tips =  document.querySelectorAll('.tip-rate');
         tips.forEach(element => element.classList.remove('active-rate'));
     }
-    function calculateTip(billAmount, tipRate, peopleCount) {
-        test = document.querySelector('.bill-amount');
-        billAmount = parseInt(document.querySelector('.bill-amount').value);    
-        peopleCount = parseInt(document.querySelector('.people-count').value)
-        tipRate = 1+ (5/100);
-        let grandTotal = billAmount * tipRate;
-        per = grandTotal/peopleCount
-        console.log(grandTotal)
-        console.log('Per: ' + per);
-        updateAmount(grandTotal, per)
-    }
+
     function updateAmount(totalAmount, perPerson) {
         //Update the calculated amount
-        document.querySelector('.tip-amount').textContent = totalAmount.toFixed('2');
-        document.querySelector('.total-amount').textContent = perPerson.toFixed('2')
+        let tip = document.querySelector('.tip-amount')
+        let total = document.querySelector('.total-amount')
+        
+        if(totalAmount == NaN || tip == undefined) {
+            return false;
+        }
+        tip.textContent = totalAmount.toFixed('2');
+        total.textContent = perPerson.toFixed('2');
     }
     function resetAmount() {
         //Clear the display tip amount
         document.querySelector('.tip-amount').textContent = '0.00';
         document.querySelector('.total-amount').textContent = '0.00';
+        resetDisable();
     }
 })
